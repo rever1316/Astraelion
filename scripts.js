2dbd478726989dcd005531c882ec3b462634d7a7
rever1316 <gustvo1598@outlook.com>
2026-07-08 16:23:40 -0300

    hospendando github-vercel

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+var MenuItem = document.querySelectorAll('.item-menu')
+
+function selectLink() {
+    MenuItem.forEach((item) =>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+MenuItem.forEach((item) =>
+    item.addEventListener('click', selectLink)
+)
+
+//Expandir Menu
+
+var btnExp = document.querySelector('btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function () {
+    menuSide.classList.toggle('abrir')
+})
